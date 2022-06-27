fb5f043043b23fbc0e7ea54c86cdea9b2beb42b7
Kacper Ledwon <you@example.com>
2022-06-27 09:08:12 +0200

    init

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
+import { UserService } from './user/user.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UserModule,
+    AuthModule,
+    PrismaModule,
+  ],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
+})
+export class AppModule {}
